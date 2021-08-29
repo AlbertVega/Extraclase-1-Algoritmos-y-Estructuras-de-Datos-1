cca57a47abd7ff96908b76ec8c2781a031a33a0a
AlbertVega <85046754+AlbertVega@users.noreply.github.com>
2021-08-28 20:12:19 -0600

    Update Servidor.java

--- Servidor.java
+++ Servidor.java
@@ -5,13 +5,19 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.*;
 
+/**
+ * @author Albert Vega Camacho
+ */
 public class Servidor {
     public static void main(String [] args) {
         VentanaServidor Ventana2 = new VentanaServidor();
     }
 }
 
-class VentanaServidor extends JFrame {
+/**
+ * Esta clase implementa los elementos de la ventana del servidor
+ */
+class VentanaServidor extends JFrame implements Runnable {
     public VentanaServidor() {
 
         setBounds(500, 300, 300, 300);
@@ -29,6 +35,51 @@ class VentanaServidor extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         setVisible(true);
+        
+        Thread Hilo1 = new Thread(this);
+
+        Hilo1.start();
+        }
+    
+        /**
+         * Esta clase gestiona los datos que llegaron al servidor del cliente
+         */
+    public void run() {  
+        try {
+            ServerSocket Socket_de_Servidor = new ServerSocket(878);
+
+            String Precio, Peso, Impuesto;
+
+            Paquetes paquete_recibido = new Paquetes();
+
+            while(true){
+
+                Socket SocketAceptador = Socket_de_Servidor.accept();
+
+                ObjectInputStream datos_entrada = new ObjectInputStream(SocketAceptador.getInputStream());
+
+                paquete_recibido = (Paquetes) datos_entrada.readObject();  
+                
+                Precio = paquete_recibido.getPrecio();
+                Peso = paquete_recibido.getPeso();
+                Impuesto = paquete_recibido.getImpuesto();
+
+                AreaTexto.append("Precio del producto = "+ Precio+"\n"+"Peso del producto = "+Peso+"\n"+"Impuesto agregado = "+Impuesto+"\n");
+
+                Socket Mensajero = new Socket("192.168.0.15", 9090);
+
+                ObjectOutputStream datos_reenviados = new ObjectOutputStream(Mensajero.getOutputStream());
+
+                datos_reenviados.writeObject(paquete_recibido);
+
+                Mensajero.close();
+
+                SocketAceptador.close();
+            }
+
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
     public JTextArea AreaTexto;
-} 
+}
