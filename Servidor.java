08d47f124ff41df45e420d2ea99843ce63307b8f
AlbertVega <85046754+AlbertVega@users.noreply.github.com>
2021-08-28 19:45:23 -0600

    Update Servidor.java

--- Servidor.java
+++ Servidor.java
@@ -1,3 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.*;
+
 public class Servidor {
-    
+    public static void main(String [] args) {
+        VentanaServidor Ventana2 = new VentanaServidor();
+    }
 }
+
+class VentanaServidor extends JFrame {
+    public VentanaServidor() {
+
+        setBounds(500, 300, 300, 300);
+
+        JPanel Lamina2 = new JPanel();
+
+        Lamina2.setLayout(new BorderLayout());
+
+        AreaTexto = new JTextArea();
+
+        Lamina2.add(AreaTexto,BorderLayout.CENTER);
+
+        add(Lamina2);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
+    public JTextArea AreaTexto;
+} 
